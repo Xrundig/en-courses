05d4ca15af2b71ff5d65abcd4506b35c08bda16c
Mykhailo Molokoiedov <mykhailo.molokoiedov@ab-soft.net>
2019-07-05 16:17:55 +0300

    preview

---
+++ js/2gis.js
@@ -0,0 +1,2 @@
+/*Подключаем дубльгис*/
+	var map = new DGWidgetLoader({"width":100,"height":400,"borderColor":"#a3a3a3","pos":{"lat":46.414746,"lon":30.723157,"zoom":16},"opt":{"city":"odessa"},"org":[{"id":"1970853117981750"}]});
--- js/script.js
+++ js/script.js
@@ -1,18 +1,77 @@
-/*$(".hero-button").click( function(event){
-	alert('hi');
-})*/
 
 
-/*Запускаем карусель с учителями*/
+
 
 $(document).ready(function(){
 	
-	// Находим блок карусели
-	var carousel = $("#carousel");
+	/*Запускаем карусель с учителями*/
+	$("#carousel").slick({
+		slidesToShow: 4,
+		infinite: true,
+		dots: false,
+		arrows: false,
+		responsive:[
+			{
+				breakpoint: 1200,
+				settings:{
+					slidesToShow: 3,
+					dots: true,
+				}
+			},
+			{
+				breakpoint: 992,
+				settings:{
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings:{
+					slidesToShow: 1
+				}
 
-	// Запускаем плагин карусели
-	carousel.owlCarousel({
-		/*singleItem: true*/
+			}
+		]
 	});
+
+	/*Запускаем карусель с ценами*/
+
+	$(".price-carousel").slick({
+		slidesToShow: 3,
+		infinite: true,
+		dots: false,
+		responsive:[
+			{
+				breakpoint: 992,
+				settings:{
+					slidesToShow: 2,
+					dots: true,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings:{
+					slidesToShow: 1,
+				}
+			}
+			
+		]
+
+
+	});
+
+	/*Плавный скролл для навигации*/
+	$(".header").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
 
 });
